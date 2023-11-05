c0a5910f5252f6b44f4f42a3d972aeccc2e6d6b9
Your Name <jesusgarcia1999@hotmail.com>
2023-11-05 22:25:53 +0000

    comentando la db local

--- core/settings.py
+++ core/settings.py
@@ -81,7 +81,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# mysql://root:dBdDG1AdHfFf53fhgdFcCbAC1DGFG3h1@viaduct.proxy.rlwy.net:52927/railway
+
+#Database Local
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
